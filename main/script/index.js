0898515cbb6e3b5ec06b53fba61a9bab59dfde3e
Ben Liu <liuben1988skku@gmail.com>
2018-02-17 20:35:33 -0800

    most recent front-end

--- main/script/index.js
+++ main/script/index.js
@@ -1,19 +1,56 @@
+document.getElementById("body_scroll").onmousewheel = function(event){
+ 
+ showMenu();   
+};
+
+function showMenu(){
+    
+ document.getElementById("navigation").style.top= 0;
+ document.getElementById("navigation").style.transition = "top 1s";
+
+   setTimeout(function(){     
+      document.getElementById("navigation").style.top= "-10%";
+ document.getElementById("navigation").style.transition = "top 1s";
+    }, 5000);      
+    
+    
+}
+
+    var ben = setInterval(function(){ 
+       console.log(document.getElementById("body_scroll").scrollTop);          
+     },1000);
+
 document.getElementById("searchBut").addEventListener("click", function(){
+    showMenu(); 
     var scTop = document.getElementById("body_scroll").scrollTop;
     var scStart = setInterval(function(){ 
         scTop = scTop+15;   
     document.getElementById("body_scroll").scrollTop = scTop;
-        if(scTop > 803){   
+        if(scTop > 904.4444580078125){   
            clearInterval(scStart); 
         }                              
      },1);
   });    
  document.getElementById("goWear").addEventListener("click", function(){
+    showMenu();  
     var scTop = document.getElementById("body_scroll").scrollTop;
     var scStart = setInterval(function(){ 
         scTop = scTop+15;   
     document.getElementById("body_scroll").scrollTop = scTop;
-        if(scTop > 2005){   
+        if(scTop > 2107.77783203125){   
+           clearInterval(scStart); 
+        }                              
+     },1);
+  
+});  
+
+document.getElementById("goWear_wear").addEventListener("click", function(){
+    showMenu();  
+    var scTop = document.getElementById("body_scroll").scrollTop;
+    var scStart = setInterval(function(){ 
+        scTop = scTop+15;   
+    document.getElementById("body_scroll").scrollTop = scTop;
+        if(scTop > 2107.77783203125){   
            clearInterval(scStart); 
         }                              
      },1);
@@ -21,17 +58,19 @@ document.getElementById("searchBut").addEventListener("click", function(){
 });  
 
  document.getElementById("goTravel").addEventListener("click", function(){
+    showMenu();  
     var scTop = document.getElementById("body_scroll").scrollTop;
     var scStart = setInterval(function(){ 
         scTop = scTop+15;   
     document.getElementById("body_scroll").scrollTop = scTop;
-        if(scTop > 3347){   
+        if(scTop > 3451.111083984375){   
            clearInterval(scStart); 
         }                              
      },1);
 });  
     
  document.getElementById("goHome").addEventListener("click", function(){
+    showMenu();  
     var scTop = document.getElementById("body_scroll").scrollTop;
     var scStart = setInterval(function(){ 
         scTop = scTop-15;   
@@ -43,6 +82,7 @@ document.getElementById("searchBut").addEventListener("click", function(){
 }); 
     
  document.getElementById("goHomeArrow").addEventListener("click", function(){
+    showMenu();  
     var scTop = document.getElementById("body_scroll").scrollTop;
     var scStart = setInterval(function(){ 
         scTop = scTop-15;   
@@ -52,4 +92,29 @@ document.getElementById("searchBut").addEventListener("click", function(){
         }                              
      },1);
 });       
-    
+
+
+
+function navigationMouse() {
+   showMenu(); 
+}
+
+
+ document.getElementById("wearBut").addEventListener("click", function(){
+  
+document.getElementById("checkWeather").style.display = "none"
+document.getElementById("checkWear").style.display = "block" 
+document.getElementById("wearBut").className = "wearBut_clicked"
+document.getElementById("weatherBut").className = "weatherBut"       
+     
+}); 
+
+ document.getElementById("weatherBut").addEventListener("click", function(){
+  
+document.getElementById("checkWeather").style.display = "block"
+document.getElementById("weatherBut").className = "weatherBut_clicked"
+document.getElementById("wearBut").className = "wearBut"     
+document.getElementById("checkWear").style.display = "none"     
+     
+}); 
+
